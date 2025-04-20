fa231d5ec82078701f61e3ae4b36131ed4d71024
liangwang <420915271@qq.com>
2025-04-20 20:32:04 +0800

    项目初始化

---
+++ code/designPattern/src/main/java/com/lw/designpattern/Demo.java
@@ -0,0 +1,12 @@
+package com.lw.designpattern;
+
+/**
+ * @author lw
+ * @create 2025-04-20-20:25
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("hahaha");
+    }
+}
